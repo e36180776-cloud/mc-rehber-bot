b3aaeefd1c759cf7afc8a4b4eb3968d57ab0214a
e36180776-cloud <e36180776@gmail.com>
2026-03-19 01:26:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+const mineflayer = require('mineflayer');
+const fs = require('fs');
+
+let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: config.host,
+        port: config.port,
+        username: config.username,
+        version: config.version
+    });
+
+    bot.on('login', () => {
+        console.log("BAŞARDIM! Bot sunucuya girdi.");
+    });
+
+    bot.on('spawn', () => {
+        console.log("Bot şu an sunucuda ayakta!");
+    });
+
+    bot.on('error', (err) => {
+        console.log("Hata oluştu: " + err);
+    });
+
+    bot.on('end', () => {
+        console.log("Bot bağlantısı koptu, tekrar deneniyor...");
+        setTimeout(createBot, 5000);
+    });
+}
+
 const http = require('http');
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bot Aktif!\n');
-}).listen(process.env.PORT || 3000);
+    res.write("Bot Aktif!");
+    res.end();
+}).listen(10000);
 
-console.log("Render Portu Acildi, Bot Minecraft'a Baglaniyor...");
+createBot();
+           
